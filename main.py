cdca3276dffced233620f03280dcb4341761cbcc
chiayewken <chiayewken@gmail.com>
2023-05-30 00:56:54 +0800

    Print intermediate task results in main.py

--- main.py
+++ main.py
@@ -21,6 +21,7 @@ def main(task_name: str, **kwargs):
         for name, task_fn in task_map.items():
             score = task_fn(**kwargs)
             results[name] = score
+            print({name: round(score * 100, 2) for name, score in results.items()})
     else:
         task_fn = task_map.get(task_name)
         if task_fn is None:
